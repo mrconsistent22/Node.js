4ed4aff61b6f88d61258903437af610e26dd11d7
mrconsistent22 <tejass4825@gmail.com>
2026-07-02 11:45:10 +0530

    http methods

--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ const url = require("url")
 
 const server = http.createServer((req, res) => {
     if(req.url === "/favicon.ico") return res.end()
-    const log = `${Date.now()}: ${req.url} New Request Received\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
     const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
+    //console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
         switch (myUrl.pathname) {
             case "/":
-                res.end("HomePage");
+                if(req.method === "GET") {
+                    res.end("HomePage");
+                }
                 break;
             case "/about":
                 const username = myUrl.query.myname
@@ -19,7 +21,16 @@ const server = http.createServer((req, res) => {
                 break;
             case '/search':
                 const search = myUrl.query.search_query;
-                res.end(`Search results for: ${search}`);
+                res.end(`Search results for: ${search}`); 
+                break;
+            case '/signup':
+                if(req.method === "GET") {
+                    res.end("This is a signup form")
+                }else if(req.method === "POST") {
+                    // DB Qyery
+                    res.end("Signup Succeded")
+                }
+                break;
             default:
                 res.end("404 Page Not Found");
         }
